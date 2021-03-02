308ab9aa2619248894b85228f59d15a88242c3ce
Pradeep LN <pradeep.ln78@gmail.com>
2021-03-02 09:57:55 +0530

    with java configuration

---
+++ src/main/java/com/training/ProductsApplication.java
@@ -0,0 +1,21 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.training.ui.ProductConsoleUI;
+
+@SpringBootApplication
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext springContainer = SpringApplication.run(ProductsApplication.class, args);
+		
+//		ProductConsoleUI ui = new ProductConsoleUI(); // DO NOT DO THIS, don't create your own instances. use the ones created by spring
+		
+		ProductConsoleUI ui = springContainer.getBean(ProductConsoleUI.class);
+		ui.createProductWithUI();
+	}
+
+}
---
+++ src/main/java/com/training/service/ProductServiceImpl.java
@@ -0,0 +1,51 @@
+package com.training.service;
+
+import java.util.List;
+
+import com.training.dal.ProductDAO;
+import com.training.dal.ProductDAOInMemImpl;
+import com.training.domain.Product;
+
+public class ProductServiceImpl implements ProductService {
+	
+	ProductDAO dao; // = new ProductDAOInMemImpl();
+	
+	public void setDao(ProductDAO dao) {
+		this.dao = dao;
+	}
+	
+	
+	@Override
+	public int addProduct(Product toBeAdded) {
+		if(toBeAdded.getPrice() * toBeAdded.getQoh() >= 10000) {
+			Product added = dao.save(toBeAdded);
+			return added.getId();
+		}else {
+			throw new IllegalArgumentException("Product Value must be GTEQ 10k");
+		}
+	}
+	
+	@Override
+	public void removeExisting(int id) {
+		Product existing = dao.findById(id);
+		
+		if(existing.getPrice() * existing.getQoh() < 100000) {
+			dao.deleteById(id);
+		}else {
+			throw new IllegalStateException("Product worth more than 100k, can't just be deleted!");
+		}
+	}
+
+	@Override
+	public Product findById(int id) {
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Product> findAll() {
+		return dao.findAll();
+	}
+
+	
+	
+}
